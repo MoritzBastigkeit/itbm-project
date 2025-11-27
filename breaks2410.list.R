7d41f0ce18f352d0dc298ef3b9fa7c9ee522829e
Valentin Ennser <h12023170@s.wu.ac.at>
2025-11-27 20:33:01 +0100

    Checked Plots and added clearer Text

---
+++ breaks2410.list.R
@@ -0,0 +1,9 @@
+# 2025-11-27 20:14:21.012561 
+options(scorecard.bin_close_right = FALSE) 
+list(
+ `status.of.existing.checking.account`=c("... < 0 DM%,%0 <= ... < 200 DM", "... >= 200 DM / salary assignments for at least 1 year", "no checking account"), 
+ `duration.in.month`=c("8", "16", "34", "44"), 
+ `credit.history`=c("no credits taken/ all credits paid back duly%,%all credits at this bank paid back duly", "existing credits paid back duly till now", "delay in paying off in the past", "critical account/ other credits existing (not at this bank)"), 
+ `savings.account.and.bonds`=c("... < 100 DM", "100 <= ... < 500 DM", "500 <= ... < 1000 DM%,%... >= 1000 DM%,%unknown/ no savings account"), 
+ `purpose`=c("retraining%,%car (used)%,%radio/television", "furniture/equipment%,%repairs", "car (new)%,%domestic appliances%,%business", "education%,%others")
+ )
